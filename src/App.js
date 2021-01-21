f6f56fe2a1f80482abba2580ea1827c6f308bfea
Luis Eduardo Gutierrez <luis.gutierrez@wizeline.com>
2021-01-21 13:29:39 -0600

    Charizard challenge accepted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+
+import PokeCard from "./components/PokeCard";
+import { mockPokemonData } from "./mock/pokeData";
+
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Welcome to your second mini challenge!</h1>
+      <h1>Click on the Pokemon for magic to happen!</h1>
+      <PokeCard {...mockPokemonData} />
     </div>
   );
 }
